0fa479821babb153989312a010852cda63d55dd5
AdiGarmendia <dejima@gmail.com>
2020-03-09 13:28:31 -0500

    names

--- Program.cs
+++ Program.cs
@@ -17,6 +17,64 @@ namespace linq {
             foreach (var fruit in LFruits) {
                 Console.WriteLine ($"{fruit}");
             }
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int> () {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where (num => {
+                return (num % 4 == 0 || num % 6 == 0);
+            });
+            foreach (var num in fourSixMultiples) {
+                Console.WriteLine ($"{num}");
+            }
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string> () {
+                "Heather",
+                "James",
+                "Xavier",
+                "Michelle",
+                "Brian",
+                "Nina",
+                "Kathleen",
+                "Sophia",
+                "Amir",
+                "Douglas",
+                "Zarley",
+                "Beatrice",
+                "Theodora",
+                "William",
+                "Svetlana",
+                "Charisse",
+                "Yolanda",
+                "Gregorio",
+                "Jean-Paul",
+                "Evangelina",
+                "Viktor",
+                "Jacqueline",
+                "Francisco",
+                "Tre"
+            };
+
+            List<string> descend = names.OrderByDescending (name => name).ToList ();
+            foreach (var name in descend) {
+                Console.WriteLine ($"{name}");
+            }
+
         }
     }
 }
